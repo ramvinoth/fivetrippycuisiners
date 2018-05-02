d8b7b401b97e20396c293b1cd761ce6585ad131e
Ram Vinoth <ramvinoth37@gmail.com>
2018-05-02 17:39:39 +0530

    youtube url embed issue fix

--- modules/posts/public/posts.services.js
+++ modules/posts/public/posts.services.js
@@ -90,7 +90,7 @@ angular.module('atwork.posts')
         };
         
         var videoId = appPostsFeed.getVideoId(text);
-        if(videoId !== 'error'){
+        if(videoId !== "" && videoId !== 'error'){
           var iframeMarkup = '<iframe width="560" height="315" src="//www.youtube.com/embed/' + videoId + '" frameborder="0" allowfullscreen></iframe>';
           text = text+"<br>"+iframeMarkup;
         }
